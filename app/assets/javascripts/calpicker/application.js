741498eef4b3b4fef9bca179aa1474514f930759
Scott Cytacki <scytacki@concord.org>
2012-09-28 11:58:56 -0400

    need to use path relative to asset root
    without this change compiling the assets fails

--- app/assets/javascripts/calpicker/application.js
+++ app/assets/javascripts/calpicker/application.js
@@ -10,5 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require calendar_date_select
+//= require calpicker/calendar_date_select
 // (locals will be handled by the helper methods ....)
