3a67b694982dc6618ffd98906f4d4751db64a13c
Doug Martin <dmartin@concord.org>
2020-08-06 10:22:59 -0500

    Updated rails dependency to use > 3.2.0

--- lib/calpicker/version.rb
+++ lib/calpicker/version.rb
@@ -1,9 +1,9 @@
 module Calpicker
   # The original VERSION constant from the plugin.
-  PLUGIN_VERSION = '1.16.2'
+  PLUGIN_VERSION = '1.16.3'
 
   # The version of the new Engine variant.
-  GEM_VERSION = "0.0.2"
+  GEM_VERSION = "0.0.3"
 
   # Support anything still looking for VERSION
   VERSION = PLUGIN_VERSION
